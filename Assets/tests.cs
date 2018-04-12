0e5f2127ac5e89a39bab67b642977941746f640c
Muhammad Moazam Ijaz <muhammadmoazamijaz@gmail.com>
2018-04-12 11:31:40 +0500

    test fetch created but webscript not available

---
+++ Assets/tests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using System.Text.RegularExpressions;
+
+public class tests : MonoBehaviour {
+
+	public string b_id;
+	public Button t_button;
+	public static string testdata;
+	private static readonly string POSTAddUserURL = "https://autismdiagnosis.000webhostapp.com/signup.php";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void testid(){
+		b_id = t_button.name;
+		WWW www;
+		WWWForm form = new WWWForm ();
+		form.AddField ("Stid", b_id);
+		www = new WWW (POSTAddUserURL, form);
+		StartCoroutine (WaitForRequest (www));
+	}
+
+	IEnumerator WaitForRequest(WWW data)
+	{
+		yield return data; // Wait until the download is done
+		if (data.error != null)
+		{
+			Debug.Log("There was an error sending request: " + data.error);
+		}
+		else
+		{
+			Debug.Log("WWW Request: " + data.text);
+		}
+	}
+
+}
