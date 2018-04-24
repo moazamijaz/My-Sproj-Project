5999716a7ed0b80b0d6678157881638ddd4df67f
Muhammad Moazam Ijaz <muhammadmoazamijaz@gmail.com>
2018-04-24 22:38:56 +0500

    children scene completed

--- Assets/childrenscript.cs
+++ Assets/childrenscript.cs
@@ -6,11 +6,13 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 public class childrenscript : MonoBehaviour {
 
 	// Use this for initialization
 	public Transform myPanel;
+	public static string cid;
 	private static readonly string POSTAddUserURL = "https://autismdiagnosis.000webhostapp.com/returnAllChildern.php";
 	void Start () {
 		loadchildren ();
@@ -27,12 +29,13 @@ public class childrenscript : MonoBehaviour {
 		form.AddField ("uid", 37); //LoginScript.userid
 		www = new WWW (POSTAddUserURL, form);
 		StartCoroutine (WaitForRequest (www));
-		/*
-		for (int i = 0; i < 50; i++) {
-			Button T = (Button)Instantiate (Resources.Load ("Child Button", typeof(Button)), myPanel);
-		}
-		*/
+	}
 
+	public void buttonclick(object sender){
+		Button button = (Button)sender;
+		cid = button.name;
+		Debug.Log ("button id: " + cid);
+		SceneManager.LoadScene (8);
 	}
 
 	IEnumerator WaitForRequest(WWW data)
@@ -41,19 +44,39 @@ public class childrenscript : MonoBehaviour {
 		if (data.error != null)
 		{
 			Debug.Log("There was an error sending request: " + data.error);
+			//StartCoroutine (WaitForRequest (data));
 		}
 		else
 		{
 			Debug.Log("WWW Request: " + data.text);
 			string[] children = data.text.Split(new[]{"<br>"},StringSplitOptions.None);
+			children = children.Take (children.Count() - 1).ToArray();
+			Debug.Log ("Children count: "+children.Count ());
 			foreach (string child in children) {
+				Button T = (Button)Instantiate (Resources.Load ("Child Button", typeof(Button)), myPanel);
 				string[] Details = child.Split ('-');
 				foreach (string detail in Details) {
 					string[] kaam = detail.Split (':');
-					Debug.Log (kaam [1]);
+					string temp=kaam[0];
+					if (temp.EndsWith("cid")) {
+						T.name = kaam [1];
+						cid = kaam [1];
+					}
+					else if (temp.EndsWith("name")) {
+						GameObject name = GameObject.Find (cid);
+						Text[] textlist = name.GetComponentsInChildren<Text> ();
+						textlist [0].text = kaam [1];
+						Debug.Log ("name: "+textlist[0].text);
+					}
+					else if (temp.EndsWith("age")) {
+						GameObject name = GameObject.Find (cid);
+						Text[] textlist = name.GetComponentsInChildren<Text> ();
+						textlist [1].text = kaam [1]+" years";
+						Debug.Log ("age: "+textlist[1].text);
+					}
 				}
+				T.onClick.AddListener (()=>buttonclick(T));
 			}
-
 		}
 	}
 }
