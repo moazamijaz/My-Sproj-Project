6fc8c790309c71b83fb3bab7896c8b1c557cfff9
Muhammad Moazam Ijaz <muhammadmoazamijaz@gmail.com>
2018-05-08 14:01:23 +0500

    Activity 3 final

--- Assets/managerActivity5.cs
+++ Assets/managerActivity5.cs
@@ -13,8 +13,8 @@ public class managerActivity5 : MonoBehaviour {
 		count = -1;
 		instructions.SetActive(false);
 		animals [0].SetActive (true);
-		animals [1].SetActive (true);
-		animals [2].SetActive (true);
+		animals [1].SetActive (false);
+		//animals [2].SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -24,10 +24,10 @@ public class managerActivity5 : MonoBehaviour {
 
 	public void next (){
 
-		if (count < 3) {
+		if (count < 2) {
 			animals [0].SetActive (true);
 			animals [1].SetActive (true);
-			animals [2].SetActive (true);
+			//animals [2].SetActive (false);
 			count++;
 			animals [count].SetActive (false);
 			instructions.SetActive(true);
--- Assets/managerActivity9.cs
+++ Assets/managerActivity9.cs
@@ -7,6 +7,9 @@ public class managerActivity9 : MonoBehaviour {
 
 	public static int score;
 	public static int count;
+	public static string cid=infoscript.cid;
+	public static string uid=LoginScript.userid;
+	private static readonly string POSTAddUserURL = "https://autismdiagnosis.000webhostapp.com/A9.php";
 
 	// Use this for initialization
 	void Start () {
@@ -18,7 +21,35 @@ public class managerActivity9 : MonoBehaviour {
 	void Update () {
 		if (count == 5) {
 		//submit the score here
+			WWW www;
+			WWWForm form = new WWWForm ();
+			form.AddField ("responseA6", "NULL");//work on this
+			form.AddField ("Child_id", cid);
+			form.AddField ("Popinion", "NULL");
+			form.AddField ("Eopinion", "NULL");
+			form.AddField ("video", "NULL");
+
+			www = new WWW (POSTAddUserURL, form);
+			StartCoroutine (WaitForRequest (www));
+			//and move the scene to menu
+			Debug.Log("move now");
 		
+		}
+	}
+
+	IEnumerator WaitForRequest(WWW data)
+	{
+		yield return data; // Wait until the download is done
+		if (data.error != null)
+		{
+			Debug.Log("There was an error sending request: " + data.error);
+			StartCoroutine (WaitForRequest (data));
+		}
+		else
+		{
+			Debug.Log("WWW Request: " + data.text);
+			//testdata = data.text;
+			Invoke ("nextscene", 1.5f);
 		}
 	}
 }
